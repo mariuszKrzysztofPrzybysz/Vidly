417069263c59221f21e675007ba907b7d2ce6dee
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-07-14 15:34:20 +0200

    Ex1

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,49 +10,65 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
+        private IEnumerable<Movie> GetMovies()
         {
-            var movie = new Movie
+            return new List<Movie>
             {
-                Name = "Shrek!"
+                new Movie {Id = 1, Name = "Pokłosie"},
+                new Movie {Id = 2, Name = "Shrek"}
             };
-
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
         }
 
-        public ActionResult Edit(int id)
+        public ActionResult Index()
         {
-            return Content($"id={id}");
+            var movies = GetMovies();
+
+            return View(movies);
         }
 
-        public ActionResult Index(int? pageIndex,string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+        //// GET: Movies/Random
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie
+        //    {
+        //        Name = "Shrek!"
+        //    };
 
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer {Name = "Customer 1"},
+        //        new Customer {Name = "Customer 2"}
+        //    };
 
-            return Content($"pageIndex={pageIndex.Value}&sortBy={sortBy}");
-        }
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
 
-        [Route(@"movies/release/{year}/{month:regex(\d{4}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content($"{year}/{month}");
-        }
+        //    return View(viewModel);
+        //}
+
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content($"id={id}");
+        //}
+
+        //public ActionResult Index(int? pageIndex,string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+
+        //    return Content($"pageIndex={pageIndex.Value}&sortBy={sortBy}");
+        //}
+
+        //[Route(@"movies/release/{year}/{month:regex(\d{4}):range(1,12)}")]
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content($"{year}/{month}");
+        //}
     }
 }
