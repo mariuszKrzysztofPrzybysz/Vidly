4d574f7a533b4284766af56a58c02da6fe15fe66
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-07-14 22:17:49 +0200

    Changing the model

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -4,6 +4,8 @@
     {
         public byte Id { get; set; }
 
+        public string Name { get; set; }
+
         public short SignUpFee { get; set; }
 
         public byte DurationInMonths { get; set; }
