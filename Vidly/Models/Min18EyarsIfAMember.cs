b4276dac0e79d0dd7cfbe8b17998c033babb90e3
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-07-17 18:16:05 +0200

    Refactoring magic numbers

--- Vidly/Models/Min18EyarsIfAMember.cs
+++ Vidly/Models/Min18EyarsIfAMember.cs
@@ -12,7 +12,8 @@ namespace Vidly.Models
             if (customer == null)
                 return new ValidationResult("Customer object is required.");
 
-            if (customer.MembershipTypeId==0 || customer.MembershipTypeId == 1)
+            if (customer.MembershipTypeId == MembershipType.Unknown
+                || customer.MembershipTypeId == MembershipType.PayAsYouGo)
                 return ValidationResult.Success;
 
             if (customer.Birthdate == null)
